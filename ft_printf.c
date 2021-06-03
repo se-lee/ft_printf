57568aa3dd77b38c4bb68d35c6032b5b679cb2e2
Seoyoung Lee <selee@z2r9p2.42lyon.fr>
2021-06-03 15:27:37 +0200

    modified set_width_prec

--- ft_printf.c
+++ ft_printf.c
@@ -1,75 +1,66 @@
 #include "ft_printf.h"
 
-void	fmt_init(t_format fmt)
+void	fmt_init(t_format *fmt)
 {
-	fmt.minus = 0;
-	fmt.zero = 0;
-	fmt.width = 0;
-	fmt.precision_dot = 0;
-	fmt.precision_value = 0;
-	fmt.type = 0;
+	fmt->minus = 0;
+	fmt->zero = 0;
+	fmt->width = 0;
+	fmt->precision_dot = 0;
+	fmt->precision_value = 0;
+	fmt->type = 0;
+	fmt->return_printf = 0;
 }
-void read_format(char *str) //return the length of input
+
+void	read_format(char *str, va_list args) 
 {
-	t_format	fmt;
+	t_format *fmt;
 	int	i;
 
 	i = 0;
-	fmt_init(fmt);
-	
-	while (str) //REVISE condition: Start after '%', but end where?  
+	while (str)  //&& when type csx... is not found
 	{
 		if (str[i] == '-')
-			fmt.minus = 1;
+			fmt->minus = 1;
 		else if (str[i] == '0')
-			fmt.zero = 1;
+			fmt->zero = 1;
 		else if (str[i] == '.')
 		{
-			fmt.precision_dot = 1; 
+			fmt->precision_dot = 1; 
 		}
-		else if (str[i] == '*' || ft_isdigit(str[i])) //consider the case just a number is present - in case where * is not used
+		else if (str[i] == '*' || ft_isdigit(str[i]))
 		{
-			fmt.width = 1; //Revise (1 or 0 or -1?)
+			set_width_precision(str[i], &fmt, args)
 		}
-//		else if (str[i] == <the specifier character>)
-// 			fmt.type = <the specifier>
+		i++;
 	}
 }
-void	set_width(t_format fmt, char c) //store the value of width to fmt.width
-{
-	int	num;
-	va_list args;
 
-	if (fmt.width == 1)
+void	set_width_precision(char c, t_format *fmt, va_list args) 
+{//a function to store value to fmt.width or fmt.precision_value
+	if (ft_isdigit((int)c))
 	{
-	//add a loop to cover if the number has more than 1 digit?
-		fmt.width *= 10 + (c - '0'); //convert char to int;
+		if (fmt->precision_dot == 0)
+			fmt->width = fmt->width * 10 + (c - '0');
+		else // if fmt->precision_dot == 1
+			fmt->precision_value = fmt->precision_value * 10 + (c - '0');
 	}
-	else if (c == '*')
+	else if (c == '*') //??
 	{
-		fmt.width = va_arg(args, int); //not sure
+		/* how to use va_arg*/
+		fmt->width = va_arg(args, int); //not sure
 	}
 }
-char	*apply_padding(t_format fmt) 
-{
+
+char	*apply_padding(t_format *fmt) 
+{//a function to fill '0' or ' '
 	char *padding;
 
-	padding = ft_calloc(fmt.width, sizeof(char));
-	if (fmt.zero == 0)	
-		ft_memset(padding, ' ', fmt.width);
+	padding = ft_calloc(fmt->width, sizeof(char));
+	if (fmt->zero == 0)	
+		ft_memset(padding, ' ', fmt->width);
 	return (padding);
 }
-void	apply_precision(t_format fmt, char c)
-{
-	if (fmt.precision_dot == 1)
-		fmt.precision_value = c - '0'; //REVISE
-}
-/*
-char	*apply_left_right()
-{
 
-}
-*/
 int	ft_type_char(int chr, t_format fmt)
 {
 	int	len;
@@ -83,6 +74,7 @@ int	ft_type_str(char *str, t_format fmt)
 	ft_putstr_fd(str, 1);
 	return (0);
 }
+/*
 int	ft_type_int(int num, t_format fmt) // itoa
 {
 	ft_putnbr_fd(num, 1);
@@ -100,28 +92,27 @@ char *ft_type_hexa(int deci)
 	// 
 	return (hexa);
 }
-int ft_type_ptr(int ptr)
-{
-//	return pointer address
-}
+*/
+
 char	*display_final(va_list args, t_format fmt)
 {
 	char	*rtn;
 	int		i;
 	
 	i = 0;
+	while ()
+
 //	fmt.return_printf ----- store the entire length of final display
-
-//	rtn[i] = 
-
+//  apply padding
+// 	apply type (conversion)
+// 	
 }
+
 int	ft_printf(const char *str, ...)
 {
 	va_list	args;
 	t_format	fmt;
 	int	i;
-	char type;
-	int	str_len;
 
 	va_start(args, str);
 	i = 0;
@@ -136,28 +127,29 @@ int	ft_printf(const char *str, ...)
 		}
 		else
 		{
-			i += 1;
-		read_format((char *)str, args);
-			if (fmt.type == 'c')
-				ft_type_char(va_arg(args, int), fmt);
-			else if (fmt.type == 's')
-				ft_type_str(va_arg(args, char*));
-			else if (fmt.type == '%')
-				ft_type_char('%', fmt);
-			else if (fmt.type == 'd' || type == 'i')
-				ft_type_int(va_arg(args, int));
+			fmt_init(&fmt);
+			read_format((char *)str);
 			i += 1;
 		}
 	}
 	va_end(args);
-	return (str_len);
+	return (/*fmt.return_printf*/);
 }
+
 
 int	main()
 {
+	t_format fmt;
 	char a;
 	a = 'a';
+	char *str;
+	str = "-5d";
+	read_format(str, &fmt);
+	printf("minus: %d", fmt.minus);
+
+/*
 	ft_printf("%c number=%d %% %c %c %s %s %s\n", a, '*', 'b', 65, "hello", "good", "bye");
 	printf("%c number=%d %% %4c %c %s %s %s\n", a, '*', 'b', 65, "hello", "good", "bye");
+*/
 	return (0);
 }
